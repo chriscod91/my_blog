c095f5732e37975cbb7f37c25f4d2c9e9e8adae5
chriscod91 <gchriscod91@gmail.com>
2021-07-19 15:35:04 -0700

    heroku

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 python3 manage.py test
 
 if [ $? -eq 0 ]; then 
-    git push heroku main 
+    git push heroku main
+    if [ $? -eq 0 ]; then 
+    heroku run python3 manage.py collectstatic 
     exit 0
 fi
 
 echo "there are errors in the code."
 exit 1
+
